e81f1fca65187da43a385f8e1bf633e916f9265a
Eonvorax <mail4bishop@gmail.com>
2024-09-05 00:14:25 +0200

    Actually forgot to push that one...

--- rain/0-rain.py
+++ rain/0-rain.py
@@ -6,20 +6,35 @@ Calculate square units of rainwater
 
 def rain(walls):
     """
+    Calculate the total amount of rainwater retained after it rains.
     """
+    if not walls:
+        return 0
+
     total_water = 0
-    prev_wall = 0
-    width = 0
-    for wall in walls:
-        if wall != 0:
-            total_water += min(prev_wall, wall) * width
-            # TODO Create a fake wall value, then add the difference to the total
-            # if wall < prev_wall and width > 0:
-            #     # total_water += prev_wall - wall
-            prev_wall = max(prev_wall, wall)
-            width = 0
+    left = 0
+    right = len(walls) - 1
+
+    # Keep track of max height so far, on left and right
+    left_max = walls[left]
+    right_max = walls[right]
+
+    # while we haven't crossed the whole structure
+    while left < right:
+        if walls[left] < walls[right]:
+            if walls[left] >= left_max:
+                left_max = walls[left]
+            else:
+                # NOTE Current wall is shorter, calculating water trapped
+                total_water += left_max - walls[left]
+            # Move left pointer to the right
+            left += 1
         else:
-            # The water's width
-            width += 1
+            if walls[right] >= right_max:
+                right_max = walls[right]
+            else:
+                total_water += right_max - walls[right]
+            # Move right pointer to the left
+            right -= 1
 
     return total_water
